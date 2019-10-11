9c39f90f4494aa4fb9caf829865cd398f9089d2b
brunsviken <brunsviken@hotmail.com>
2019-10-12 00:09:45 +0200

    test commit nr 3

--- sample.c
+++ sample.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main ()
 {
-	printf("hello der hjemme");
-	printf("Edit nr 2 fra hjemme");
+	printf("hello");
+	printf("Edit nr 2");
+	printf("edit nr 3 from home");
 	return 0;
 }
 
